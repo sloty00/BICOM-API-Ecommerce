bb41800be37298c52e1fc567c0f79c15ccfaf610
Jose Vargas <arnoldovargas1981@gmail.com>
2022-03-16 18:37:31 -0300

    Algo asi...

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const indexRouter = require('./router/router');
-
 const app = express();
+
+// Parseo del Body de Cada Petición
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
 
 app.use('/api', indexRouter); //<--- (http://localhost/api)
 
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,17 @@
-var mysql = require('mysql2');
-var pool  = mysql.createPool({
-    connectionLimit : 10,
-    host: "localhost",
-    user: "root",
-    password: "#Root2022",
-    database: "bicom2",
-    multipleStatements: true
-  });
+const mysql = require('mysql2');
+require('dotenv').config();
+
+let pool;
+pool = mysql.createPool({
+  connectionLimit : 10,
+  host     : process.env.BC_HOST || 'localhost',
+  user     : process.env.BC_USER || 'root',
+  password : process.env.BC_PASSWORD || '#Root2022',
+  database : process.env.BC_DATABASE || 'bicom2',
+  multipleStatements: true
+});
 
 module.exports = pool;
+
+
+//connection.end();  <--- Finaliza la conexion.
